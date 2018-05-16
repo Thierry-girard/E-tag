ec6200f15d0ca759e697b64847fd08a7326b2659
Thierry Girard <girard.thierry80@gmail.com>
2018-05-16 17:23:26 +0200

    Moving the project to github.

---
+++ Assets/ParticleHit.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ParticleHit : MonoBehaviour {
+
+	public GameObject collisionResult;
+
+	public GameObject paintingPrefab;
+
+    public GameObject selectedColor;
+
+	private Dictionary<string,GameObject> paintingObject;
+
+	private ParticleSystem playerParticle;
+
+	private Color quadColor;
+
+    //TODO
+    //Réduire nb particules, réduire radius
+    //Voir pb luminosité quad
+    //Récupérer position main
+
+	// Use this for initialization
+	void Start () {
+		playerParticle = gameObject.GetComponent<ParticleSystem> ();
+		quadColor = Color.red;
+		paintingObject = new Dictionary<string,GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		quadColor = selectedColor.GetComponent<CanvasRenderer>().GetColor();
+
+        ParticleSystem.MainModule particleModule = playerParticle.main;
+        particleModule.startColor = selectedColor.GetComponent<CanvasRenderer>().GetColor();
+	}
+
+	private GameObject GetParentObject(string color) {
+		if (!paintingObject.ContainsKey (color)) {
+
+			GameObject painting = Instantiate<GameObject>(paintingPrefab);
+			painting.GetComponent<MeshFilter>().mesh = new Mesh();
+            Color outColor;
+            ColorUtility.TryParseHtmlString(color,out outColor);
+            painting.name = "Painting " + color;
+            painting.GetComponent<MeshRenderer>().material.color = outColor;
+
+            paintingObject[color] = painting;
+		}
+		return paintingObject [color];
+	}
+
+    public void OnParticleCollision(GameObject other)
+	{
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+
+        int numCollisionEvents = playerParticle.GetCollisionEvents(other, collisionEvents);
+
+        for (int i = 0; i < numCollisionEvents; i++)
+        {
+            Vector3 collisionHitLoc = collisionEvents[i].intersection;
+
+            GameObject parentPainting = GetParentObject(ColorUtility.ToHtmlStringRGB(quadColor));
+            //Create quad
+            GameObject result = collisionResult;
+            //Position
+            result.transform.position = collisionHitLoc;
+            //Rotation
+            result.transform.LookAt(collisionHitLoc + -1 * collisionEvents[i].normal);
+            //Set color and assign to parent
+            result.transform.parent = parentPainting.transform;
+
+            //Children
+            var resultMesh = result.GetComponent<MeshFilter>();
+            CombineInstance combine = new CombineInstance();
+            combine.mesh = resultMesh.sharedMesh;
+            combine.transform = result.transform.localToWorldMatrix * parentPainting.transform.worldToLocalMatrix;
+
+            //Parent
+            var paintingMesh = parentPainting.GetComponent<MeshFilter>();
+            CombineInstance current = new CombineInstance();
+            current.mesh = paintingMesh.sharedMesh;
+            current.transform = parentPainting.transform.localToWorldMatrix;
+
+            paintingMesh.sharedMesh = new Mesh();
+            paintingMesh.sharedMesh.CombineMeshes(new CombineInstance[] { combine, current });
+
+            paintingMesh.GetComponent<MeshRenderer>().material.color = quadColor;
+
+            //Destroy(result);
+        }
+	}
+}
+
---
+++ Assets/ParticlesOnClick.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity.InputModule;
+
+public class ParticlesOnClick : MonoBehaviour, IManipulationHandler, IHoldHandler, IInputHandler
+{
+
+	public float distance = 0;
+
+	public ParticleSystem playerParticle;
+
+    private bool fireParticle;
+
+    private InputEventData holdEvent;
+
+    public void OnHoldCanceled(HoldEventData eventData)
+    {
+        Debug.Log("Hold canceled");
+    }
+
+    public void OnHoldCompleted(HoldEventData eventData)
+    {
+        Debug.Log("Hold completed");
+        fireParticle = false;
+    }
+
+    public void OnHoldStarted(HoldEventData eventData)
+    {
+        Debug.Log("Hold started");
+        fireParticle = true;
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        Debug.Log("Input down");
+        holdEvent = eventData;
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        Debug.Log("Input up");
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+        Debug.Log("Manipulation canceled");
+        fireParticle = false;
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+        Debug.Log("Manipulation completed");
+        fireParticle = false;
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+        Debug.Log("Manipulation started");
+        fireParticle = true;
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        Debug.Log("Manipulation updated");
+        fireParticle = true;
+    }
+
+
+
+    // Use this for initialization
+    void Start () 
+	{
+        ParticleSystem.EmissionModule playerEmission = playerParticle.emission;
+        playerEmission.enabled = false;
+        fireParticle = false;
+        holdEvent = null;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (fireParticle) {
+            Vector3 systemPosition = Camera.main.transform.position;
+            Quaternion systemRotation = Camera.main.transform.rotation;
+            if (holdEvent != null)
+            {
+               // Debug.Log("HoldEvent not null");
+                IInputSource currentInputSource = holdEvent.InputSource;
+                uint currentInputSourceId = holdEvent.SourceId;
+
+                Vector3 handPosition;
+                if (currentInputSource.TryGetGripPosition(currentInputSourceId, out handPosition))
+                {
+                    handPosition += new Vector3(0, 0.03f, 0) ;
+                    systemPosition = handPosition;
+
+                    Vector3 heading = handPosition - Camera.main.transform.position;
+                    float distance = heading.magnitude;
+                    Vector3 direction = new Vector3(0, 0, 0);
+
+                    if(distance != 0)
+                        direction = heading / distance;
+
+                    systemRotation = Quaternion.LookRotation(direction);
+                }
+            }
+            ParticleSystem.EmissionModule playerEmission = playerParticle.emission;
+            playerEmission.enabled = true;
+            playerParticle.transform.position = systemPosition;
+			playerParticle.transform.rotation = systemRotation;
+		} else {
+            ParticleSystem.EmissionModule playerEmission = playerParticle.emission;
+            playerEmission.enabled = false;
+		}
+	}
+}
